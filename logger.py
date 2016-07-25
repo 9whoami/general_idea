aa72bae4df30177cdf4f6696af4a86b3b11cde8a
wiom <xwiomx@icloud.com>
2016-07-26 03:48:18 +0400

    fix logger

--- logger.py
+++ logger.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import logging
+import inspect
+import os
+
 from os.path import isfile
 from datetime import datetime
 from config import Conf
 
 __author__ = 'whoami'
-__version__ = '0.0.0'
+__version__ = '1.0.1'
 __date__ = '27.03.16 0:46'
 __description__ = """
 Оборачиваем работу проекта в лог
@@ -13,6 +16,42 @@ __description__ = """
 
 config = Conf()
 config.read_section('logger')
+
+
+def get_class_variable_state(fun):
+    def wrapper(cls):
+        head_line = '*' * 5 + ' {0} varible state '.format(cls.__class__) + '*' * 5
+        foter_line = '*' * len(head_line)
+        body_line = ''
+        dct = dict(map(lambda x: (x, getattr(cls, x)),
+                       filter(lambda x: '__' not in x and isinstance(getattr(cls, x), (tuple, list, int, str, set, dict)),
+                              cls.__dir__())))
+        for key in dct:
+            body_line += '{0} : {1}\n'.format(key, dct[key])
+
+        return '\n{0}\n{1}\n{2}'.format(head_line, body_line, foter_line)
+
+    return wrapper
+
+
+def call_info(fun):
+
+    def wrapper(self, *args, **kwargs):
+        result = None
+        stack = inspect.stack()
+        info = '\nCaller: {}\nCalling: {}\nargs: {}\nkwargs: {}'.format(
+            stack[1][3], str(fun), str(args), str(kwargs))
+        try:
+            result = fun(self, *args, **kwargs)
+        except Exception as e:
+            Logger().critical(info, '\nException: {}'.format(e))
+        else:
+            Logger().info(info, '\nResult: {}'.format(result))
+        finally:
+            return result
+
+    return wrapper
+
 
 class SingletonMetaclass(type):
     """
@@ -30,9 +69,11 @@ class SingletonMetaclass(type):
 
 
 class Logger(metaclass=SingletonMetaclass):
+    variable = '123'
+
     def __init__(self):
-        log_msg_format = '%(asctime)s.%(msecs)d %(levelname)s in ' \
-                         '\'%(module)s\' at line %(lineno)d: %(message)s'
+        # '%(module)s' at line %(lineno)d:
+        log_msg_format = "%(asctime)s.%(msecs)d %(levelname)s in %(message)s"
 
         level = logging.DEBUG if int(config.debug) else logging.INFO
 
@@ -59,17 +100,29 @@ class Logger(metaclass=SingletonMetaclass):
             except Exception:
                 pass
 
-    def info(self, msg):
-        self.logger.info(msg)
+    @staticmethod
+    def get_call_info():
+        stack = inspect.stack()
+        module = os.path.basename(stack[2][1])
+        line = stack[2][2]
+        return module, line
 
-    def error(self, msg):
-        self.logger.error(msg)
+    def info(self, *msg):
+        self.logger.info("'{}' at line {}: ".format(*self.get_call_info()) +
+                         ', '.join(map(lambda x: str(x), msg)))
 
-    def warning(self, msg):
-        self.logger.warning(msg)
+    def error(self, *msg):
+        self.logger.error("'{}' at line {}: ".format(*self.get_call_info()) +
+                          ', '.join(map(lambda x: str(x), msg)))
 
-    def critical(self, msg):
-        self.logger.critical(msg)
+    def warning(self, *msg):
+        self.logger.warning("'{}' at line {}: ".format(*self.get_call_info()) +
+                            ', '.join(map(lambda x: str(x), msg)))
 
-    def debug(self, msg):
-        self.logger.debug(msg)
+    def critical(self, *msg):
+        self.logger.critical("'{}' at line {}: ".format(*self.get_call_info()) +
+                             ', '.join(map(lambda x: str(x), msg)))
+
+    def debug(self, *msg):
+        self.logger.debug("'{}' at line {}: ".format(*self.get_call_info()) +
+                          ', '.join(map(lambda x: str(x), msg)))
--- main.py
+++ main.py
@@ -2,21 +2,20 @@
 # -*- coding: utf-8 -*-
 from antigate import AntiGateError
 from grab.error import GrabTimeoutError
-from random import shuffle
 from fake_useragent import UserAgent
 from time import sleep
+from random import shuffle, randint
 from selenium.common.exceptions import StaleElementReferenceException, WebDriverException
 from selenium.webdriver.common.keys import Keys
 
-from random import randint
 from browser import WebDriver
-from logger import Logger
-from config import Conf
-from threadpool import ThreadPool
 from captha_lib import RecognizeCaptcha
+from config import Conf
+from logger import Logger, call_info, get_class_variable_state
+from threadpool import ThreadPool
 
 __author__ = "whoami"
-__version__ = "0.0.1"
+__version__ = "1.0.2"
 __date__ = "09.07.16 17:56"
 __description__ = """"""
 
@@ -25,16 +24,25 @@ class SearchInGoogle:
     search_result = None
     target_site = None
 
+    @call_info
     def __init__(self, driver):
-        self.driver = driver
-        self.page_cnt = 0
-        self.site_cnt = 0
-        self.gen_result_limit()
-        self.page_limit = randint(
-            int(config.page_limit.split(',')[0]),
-            int(config.page_limit.split(',')[1]))
-        self.driver.get('https://www.google.com')
+        try:
+            self.driver = driver
+            self.page_cnt = 0
+            self.site_cnt = 0
+            self.gen_result_limit()
+            self.page_limit = randint(
+                int(config.page_limit.split(',')[0]),
+                int(config.page_limit.split(',')[1]))
+            self.driver.get('https://www.google.com')
+        except Exception as e:
+            logger.error('It rases an exception with message: {!r}'.format(str(e)))
 
+    @get_class_variable_state
+    def __str__(self):
+        pass
+
+    @call_info
     def captcha(self):
         captcha_img = 'html/body/div/img'
         captcha_input = ".//*[@id='captcha']"
@@ -57,20 +65,26 @@ class SearchInGoogle:
         else:
             return True
 
+    @call_info
     def gen_result_limit(self):
         self.site_cnt = 0
         self.result_limit = randint(
             int(config.site_limit.split(',')[0]),
             int(config.site_limit.split(',')[1]))
+        logger.info(self)
 
+    @call_info
     def search(self, search_request):
-        self.driver.filling_web_element(".//*[@id='lst-ib']", search_request)
-        sleep(1)
-        self.driver.find_element_by_css_selector("body").send_keys(Keys.RETURN)
+        # self.driver.filling_web_element(".//*[@id='lst-ib']", search_request)
+        # sleep(1)
+        # self.driver.find_element_by_css_selector("body").send_keys(Keys.RETURN)
+
         input_field = self.driver.get_element_or_none(".//*[@id='lst-ib']")
         self.driver.filling_web_element(input_field, search_request)
+        sleep(2)
         input_field.submit()
 
+    @call_info
     def collect_result(self):
         search_results = ".//*[@id='rso']/.//div[@class='rc']/h3[@class='r']/a"
         self.search_result = self.driver.get_elements_by_xpath(search_results)
@@ -79,9 +93,12 @@ class SearchInGoogle:
             if config.target_domain in href:
                 self.target_site = web_elem
                 break
+        logger.info(self)
 
+    @call_info
     def go_rand_result(self):
         while True:
+            logger.info(self)
             try:
                 if self.site_cnt >= self.result_limit:
                     raise IndexError
@@ -112,6 +129,7 @@ class SearchInGoogle:
                 continue
             yield href
 
+    @call_info
     def go_to_next_page(self):
         if self.page_cnt >= self.page_limit:
             return False
@@ -121,13 +139,20 @@ class SearchInGoogle:
         self.gen_result_limit()
         next_page = ".//*[@id='pnnext']"
         next_btn = self.driver.get_element_or_none(next_page)
+        logger.info(self)
         return self.driver.btn_click(next_btn)
 
 
 class ViewSite:
+    @call_info
     def __init__(self, driver):
         self.driver = driver
 
+    @get_class_variable_state
+    def __str__(self):
+        pass
+
+    @call_info
     def scroll_down(self):
         timer(randint(
             int(config.freeze_time.split(',')[0]),
@@ -136,11 +161,13 @@ class ViewSite:
             sleep(randint(1, 2))
             self.driver.scroll_down(500)
 
+    @call_info
     def close(self):
         self.driver.close_current_tab()
 
 
 class ViewTargetSite:
+    @call_info
     def __init__(self, driver):
         self.driver = driver
         self.depth_max = randint(
@@ -148,6 +175,11 @@ class ViewTargetSite:
             int(config.depth.split(',')[1]))
         self.depth_cur = 0
 
+    @get_class_variable_state
+    def __str__(self):
+        pass
+
+    @call_info
     def scroll_down(self):
         timer(randint(
             int(config.freeze_time.split(',')[0]),
@@ -157,11 +189,14 @@ class ViewTargetSite:
             sleep(randint(1, 2))
             self.driver.scroll_down(500)
 
+    @call_info
     def collect_lnk(self):
         self.links = self.driver.get_elements_by_xpath("//*[@href]")
 
+    @call_info
     def go_to_rnd_lnk(self):
         while True:
+            logger.info(self)
             self.collect_lnk()
             try:
                 if self.depth_cur >= self.depth_max:
@@ -179,81 +214,121 @@ class ViewTargetSite:
             self.depth_cur += 1
             yield
 
+    @call_info
     def close(self):
         self.driver.close_current_tab()
 
 
-fake_ua = UserAgent()
-th_pool = ThreadPool(max_threads=1)
-config = Conf()
-config.read_section('base')
-driver = None
+def read_search_requests():
+    logger.info('Reading search requests list...')
+
+    try:
+        with open('search_requests', 'r') as f:
+            search_requests = f.read().split('\n')
+        shuffle(search_requests)
+    except Exception as e:
+        logger.error('It raises an exception with message: {!r}'.format(str(e)))
+        raise SystemExit
+    else:
+        logger.info('Reading search requests list...OK')
+
+    return search_requests
+
+logger = Logger()
+logger.info("Initialization...")
+try:
+    fake_ua = UserAgent()
+    th_pool = ThreadPool(max_threads=1)
+    config = Conf()
+    config.read_section('base')
+    driver = None
+
+    logger.info('Reading proxy list...')
+    try:
+        with open('proxy', 'r') as f:
+            proxy_list = f.read().split('\n')
+    except Exception as e:
+        logger.error('It raises an exception with message: {!r}'.format(str(e)))
+        raise SystemExit
+    else:
+        logger.info('Reading proxy list...OK')
+
+    search_requests = read_search_requests()
+except BaseException as e:
+    logger.critical('Initialization raises an exception with message: {!r}'.format(str(e)))
+    raise SystemExit
+else:
+    logger.info('Initialization...OK')
 
 
 @th_pool.thread
 def timer(seconds):
+    logger.info('Sleep {} seconds'.format(seconds))
     for _ in range(0, seconds):
         sleep(1)
 
-with open('proxy', 'r') as f:
-    proxy_list = f.read().split('\n')
-
-
-def read_search_requests():
-    with open('search_requests', 'r') as f:
-        search_requests = f.read().split('\n')
-    shuffle(search_requests)
-    return search_requests
-
-search_requests = read_search_requests()
-
 while True:
     try:
-        request = search_requests.pop()
-    except IndexError:
-        search_requests = read_search_requests()
-        continue
-
-    while True:
-        # proxy = None
         try:
-            proxy = proxy_list.pop()
-            if proxy == '':
-                raise IndexError
+            logger.info('Receiving a search request...')
+            request = search_requests.pop()
         except IndexError:
-            Logger().error('Закончились прокси')
-            raise SystemExit
-
-        try:
-            driver = WebDriver(user_agent=fake_ua.random, proxy=proxy, proxy_type=config.proxy_type)
-        except Exception as e:
-            print(e)
+            logger.info('Request list is empty')
+            search_requests = read_search_requests()
             continue
         else:
-            break
+            logger.info('Receiving a search request...OK')
 
-    sig = SearchInGoogle(driver)
-    try:
-        if not sig.captcha():
-            raise AttributeError
-        sig.search(request)
-    except AttributeError:
-        driver.close()
-        continue
-    sleep(2)
-    sig.collect_result()
+        while True:
+            proxy = None
+            # try:
+            #     logger.info('Receiving a proxy...')
+            #     proxy = proxy_list.pop()
+            #     if proxy == '':
+            #         raise IndexError
+            # except IndexError:
+            #     logger.error('Proxy list is empty...')
+            #     raise SystemExit
+            # else:
+            #     logger.info('Receiving a proxy...OK')
 
-    for url in sig.go_rand_result():
-        if config.target_domain in url:
-            vts = ViewTargetSite(driver)
-            vts.scroll_down()
-            for _ in vts.go_to_rnd_lnk():
+            logger.info('Starting the Web driver...')
+            try:
+                driver = WebDriver(user_agent=fake_ua.random, proxy=proxy, proxy_type=config.proxy_type)
+            except Exception as e:
+                logger.error('Starting the Web driver raises an exception with a message: {!r}'.format(str(e)))
+                continue
+            else:
+                logger.info('Starting the Web driver...OK')
+                break
+
+        sig = SearchInGoogle(driver)
+        try:
+            if not sig.captcha():
+                raise AttributeError
+            sig.search(request)
+        except AttributeError:
+            driver.close()
+            continue
+        sleep(2)
+
+        sig.collect_result()
+
+        for url in sig.go_rand_result():
+            if config.target_domain in url:
+                vts = ViewTargetSite(driver)
                 vts.scroll_down()
-            vts.close()
-            break
-        else:
-            vs = ViewSite(driver)
-            vs.scroll_down()
-            vs.close()
+                for _ in vts.go_to_rnd_lnk():
+                    vts.scroll_down()
+                vts.close()
+                break
+            else:
+                vs = ViewSite(driver)
+                vs.scroll_down()
+                vs.close()
 
-    driver.close()
+        logger.info('The work is finished, close the Web Driver')
+        driver.close()
+    except KeyboardInterrupt:
+        logger.info('Job canceled by the user')
+        raise SystemExit
